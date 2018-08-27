c1df80d0839ae6f472a93bb36c0266e7f5eac873
Hugo Landim <hugo.landim.santos@accenture.com>
2018-08-27 09:21:54 -0300

    Some updates

--- EventForm.js
+++ EventForm.js
@@ -3,19 +3,71 @@ import {
     View,
     Text,
     TouchableHighlight,
+    TextInput,
+    StyleSheet
 } from 'react-native';
+import DateTimePicker from 'react-native-datetime-picker';
+
+const styles = StyleSheet.create({
+    fieldContainer: {
+        marginTop: 20,
+        marginBottom: 20,
+        backgroundColor: '#fff'
+    },
+    text: {
+        height: 40,
+        margin: 0,
+        marginRight: 7,
+        marginLeft: 10,
+    },
+    button: {
+        height: 50,
+        backgroundColor: '#48BBEC',
+        borderColor: '#48BBEC',
+        alignSelf: 'stretch',
+        margin: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 18,
+    }
+});
 
 class EventForm extends React.Component {
 
+    state = {
+        title: ""
+    }
+
+    handleChangeTitle = (value) => {
+        this.setState({title: value});
+    }
+
     handleAddPress = () => {
+        console.log(this.state.title);
+        //this.props.navigation.navigate('list');
     }
 
     render() {
         return (
-            <View>
+            <View style={{flex: 1}}>
+                <View style={styles.fieldContainer}>
+                    <TextInput style={styles.text}
+                        placeholder="Event title"
+                        spellCheck={false}
+                        value={this.state.title}
+                        onChangeText={this.handleChangeTitle}/>
+                    <TextInput 
+                    style={[styles.text, styles.borderTop]}/>
+                </View>
                 <TouchableHighlight
+                    style={styles.button}
                     onPress={this.handleAddPress}>
-                    <Text>add</Text>
+                    <Text
+                        style={styles.buttonText}>Add</Text>
                 </TouchableHighlight>
             </View>
         );
